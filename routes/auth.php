ddccdebb9d8560884cfa5d95a73a52d467374c5d
Dmscndraa <dimascndraa18@gmail.com>
2024-02-29 09:38:33 +0700

    Update

--- routes/auth.php
+++ routes/auth.php
@@ -17,9 +17,9 @@ Route::middleware('guest')->group(function () {
 
     // Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+    Route::get('/bukan-login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
-    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('/bukan-login', [AuthenticatedSessionController::class, 'store']);
 
     // Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
     //     ->name('password.request');
