27bca368675e09d5335fd84caa40ab2056c2217a
unknown <Dimascndraa>
2024-06-13 11:02:43 +0700

    memperbaiki urutan dokter spesialis

--- resources/views/alldokter.blade.php
+++ resources/views/alldokter.blade.php
@@ -11,22 +11,25 @@
     <section class="doctors pt-5 overflow-hidden bg-white" id="doctors">
         @foreach ($sortedDokters as $jabatan => $doktersGroup)
             @if ($doktersGroup->isNotEmpty())
-                <div class="mb-5">
+                <div class="container">
                     <h1 class="heading pt-5" style="font-size: 18pt; text-align: left"><span>{{ $jabatan }}</span></h1>
-                    <div class="box-container">
+                    <div class="row">
                         @foreach ($doktersGroup as $d)
-                            <a href="/dokter/{{ $d->id }}" class="nav-link">
-                                <div class="box p-5" data-aos="fade-up">
-                                    <img src="{{ asset('storage/' . $d->foto) }}" alt="">
-                                    <h4>{{ $d->name }}</h4>
-                                    <span style="color: #e97f0d">{{ $d->jabatan }}</span>
-                                </div>
-                            </a>
+                            <div class="col-md-4 mb-4">
+                                <a href="/dokter/{{ $d->id }}" class="nav-link">
+                                    <div class="box p-5" data-aos="fade-up">
+                                        <img src="{{ asset('storage/' . $d->foto) }}" alt="">
+                                        <h4>{{ $d->name }}</h4>
+                                        <span style="color: #e97f0d">{{ $d->jabatan }}</span>
+                                    </div>
+                                </a>
+                            </div>
                         @endforeach
                     </div>
                 </div>
             @endif
         @endforeach
     </section>
+
     <!-- doctors section ends -->
 @endsection
