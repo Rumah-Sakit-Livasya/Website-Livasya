4358b6f7b565ecf58bc0845ed7ad54526c8a409c
unknown <Dimascndraa>
2024-06-13 10:57:32 +0700

    memperbaiki urutan dokter spesialis

--- resources/views/alldokter.blade.php
+++ resources/views/alldokter.blade.php
@@ -11,17 +11,19 @@
     <section class="doctors pt-5 overflow-hidden bg-white" id="doctors">
         @foreach ($sortedDokters as $jabatan => $doktersGroup)
             @if ($doktersGroup->isNotEmpty())
-                <h1 class="heading pt-5" style="font-size: 18pt"><span>{{ $jabatan }}</span></h1>
-                <div class="box-container">
-                    @foreach ($doktersGroup as $d)
-                        <a href="/dokter/{{ $d->id }}" class="nav-link">
-                            <div class="box p-5" data-aos="fade-up">
-                                <img src="{{ asset('storage/' . $d->foto) }}" alt="">
-                                <h4>{{ $d->name }}</h4>
-                                <span style="color: #e97f0d">{{ $d->jabatan }}</span>
-                            </div>
-                        </a>
-                    @endforeach
+                <div class="mb-5">
+                    <h1 class="heading pt-5" style="font-size: 18pt; text-align: left"><span>{{ $jabatan }}</span></h1>
+                    <div class="box-container">
+                        @foreach ($doktersGroup as $d)
+                            <a href="/dokter/{{ $d->id }}" class="nav-link">
+                                <div class="box p-5" data-aos="fade-up">
+                                    <img src="{{ asset('storage/' . $d->foto) }}" alt="">
+                                    <h4>{{ $d->name }}</h4>
+                                    <span style="color: #e97f0d">{{ $d->jabatan }}</span>
+                                </div>
+                            </a>
+                        @endforeach
+                    </div>
                 </div>
             @endif
         @endforeach
