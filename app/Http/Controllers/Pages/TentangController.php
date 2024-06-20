fd303ede890d7c66217baefa654b7cfba41a1f77
unknown <Dimascndraa>
2024-06-20 15:28:54 +0700

    error career

--- app/Http/Controllers/Pages/TentangController.php
+++ app/Http/Controllers/Pages/TentangController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Pages;
 
 use App\Models\Identity;
+use App\Models\Mitra;
 use App\Models\Pelayanan;
 use App\Models\Timeline;
 use Illuminate\Routing\Controller;
@@ -14,8 +15,10 @@ class TentangController extends Controller
         $identity = Identity::first();
         $pelayanan = Pelayanan::all();
         $timelines = Timeline::all();
+        $mitras = Mitra::where('is_primary', 1)->get();
 
         return view('about.index', compact('pelayanan', 'timelines'), [
+            'mitras' => $mitras,
             'title' => 'Tentang Kami',
             'identity' => $identity,
         ]);
