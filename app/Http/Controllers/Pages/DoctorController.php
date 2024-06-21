2ea5274ea9d035927de9d3e747c1062fb5f921c7
unknown <Dimascndraa>
2024-06-21 14:52:19 +0700

    menambahkan departement

--- app/Http/Controllers/Pages/DoctorController.php
+++ app/Http/Controllers/Pages/DoctorController.php
@@ -11,7 +11,7 @@ class DoctorController extends Controller
 {
     public function index()
     {
-        $doctors = Doctor::all();
+        $doctors = Doctor::orderBy('urutan', 'asc')->get();
 
         return view('pages.doctors.index', compact('doctors'), [
             'title' => 'Dokter'
