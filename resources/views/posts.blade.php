95f9945ae641efdbd6137be031a815015b6569e6
Dimascndraa <dimascndraa18@gmail.com>
2025-01-22 11:13:34 +0700

    perbaikan berita

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -38,12 +38,17 @@
                     <div class="col-lg-4">
                         <a href="/posts/{{ $post->slug }}" class="text-decoration-none">
                             <div class="card img-parent overflow-hidden position-relative shadow"
-                                style="border-radius: 20px; outline: none; border: none;">
+                                style=" outline: none; border: none;">
+                                <div class="card-header bg-white" style="border: none; display: flex; align-items: center;">
+                                    <img src="{{ asset('img/ig.jpg') }}" alt="Image"
+                                        style="width: 30px; height: 30px; border-radius: 50%; margin-right: 5px; vertical-align: middle;">
+                                    <div style="display: flex; flex-direction: column; line-height: 1.2; font-size: 12px;">
+                                        <strong class="mb-1">rslivasya</strong>
+                                        <p class="mb-0">Majalengka</p>
+                                    </div>
+                                </div>
                                 @if ($post->image)
                                     <div class="card-img-top img overflow-hidden">
-                                        <p class="text-muted"
-                                            style="font-size: 0.9rem; margin: 0; position: absolute; top: 10px; left: 10px; background-color: rgba(255, 255, 255, 0.7); padding: 5px; border-radius: 5px;">
-                                            @rslivasya</p>
                                         <div
                                             style="background-image: url({{ asset('/storage/' . $post->image) }}); background-size: cover; height: 400px; background-position: center;">
                                         </div>
@@ -61,14 +66,24 @@
                                 <div class="card-body text-center" style="background-color: #fff; padding: 1rem;">
                                     <h5 class="card-title" style="font-size: 1.2rem; font-weight: bold;">{{ $post->title }}
                                     </h5>
-                                    <p class="card-text" style="font-size: 0.9rem; color: #6c757d;"><i
-                                            class="far fa-folder-open" style="color:#0d6efd;"></i>
-                                        {{ $post->category->name }}</p>
-                                    <div class="d-flex justify-content-between align-items-center mt-2">
-                                        <span class="text-muted">{{ $tanggal }} {{ $bulan }}</span>
-                                        <span class="text-muted"><i class="far fa-heart"></i>
-                                            {{ $post->likes_count }}</span>
+                                    <div class="d-flex justify-content-start align-items-center mt-5" style="gap: 10px;">
+                                        <span class="text-muted"><img
+                                                src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
+                                                width="24"></span>
+                                        <span class="text-muted" style="opacity: 0.7;"><img
+                                                src="https://cdn-icons-png.flaticon.com/128/5948/5948565.png"
+                                                width="24"></span>
+                                        <span class="text-muted" style="opacity: 0.7; transform: rotate(-90deg)"><img
+                                                src="https://cdn-icons-png.flaticon.com/128/1286/1286853.png"
+                                                width="24"></span>
                                     </div>
+                                    <p class="text-muted mt-2 text-left">99k likes</p>
+                                </div>
+                                <hr>
+                                <div class="add-comment" style="height: 30px; display: flex; align-items: center;">
+                                    <h6 class="ml-3">Add a Comment</h6>
+                                    <img src="https://cdn-icons-png.flaticon.com/128/1384/1384031.png" class="mr-3 mb-3"
+                                        alt="Instagram Icon" style="width: 24px; height: 24px; margin-left: auto;">
                                 </div>
                             </div>
                         </a>
