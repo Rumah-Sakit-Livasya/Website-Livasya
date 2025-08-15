4a6da130eb30124bb0aec57742242feaeaaac0a5
FrahestaYA <tiyasfrahesta1@gmail.com>
2025-08-15 10:30:26 +0700

    add livasya league

---
+++ resources/views/livasya-league.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('container')
+    <div class="league-gallery"></div>
+
+    <section class="title" style="background: var(--primary);">
+        <h1 class="fw-bold text-light" data-aos="fade-right" data-aos-anchor-placement="top-bottom">Livasya League</h1>
+    </section>
+
+    <section class="league bg-white" style="padding-top: 5rem">
+        <div class="container">
+            <iframe src="https://copafacil.com/-c2p4o" width="100%" height="550" frameborder="0"></iframe>
+        </div>
+    </section>
+@endsection
